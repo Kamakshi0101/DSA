96e3298700e161451d57c7aa0ec1a524a2f929a3
Kamakshi Aggarwal <138430584+Kamakshi0101@users.noreply.github.com>
2026-02-19 10:14:55 +0530

    Time: 5 ms (58.7%), Space: 101.2 MB (69.6%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.java
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.java
@@ -9,21 +9,54 @@
  * }
  */
 class Solution {
-    public int pairSum(ListNode head) {
-        List<Integer> list= new ArrayList<>();
-        while(head!=null)
+    public ListNode reverse(ListNode node)
+    {
+        ListNode prev=null;
+        ListNode curr=node;
+        while(curr!=null)
         {
-            list.add(head.val);
-            head=head.next;
+            ListNode temp= curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=temp;
         }
-        int i=0,j=list.size()-1;
-        int max= Integer.MIN_VALUE;
-        while(i<j)
+        return prev;
+    }
+    public int pairSum(ListNode head) {
+        // List<Integer> list= new ArrayList<>();
+        // while(head!=null)
+        // {
+        //     list.add(head.val);
+        //     head=head.next;
+        // }
+        // int i=0,j=list.size()-1;
+        // int max= Integer.MIN_VALUE;
+        // while(i<j)
+        // {
+        //     int sum=list.get(i)+list.get(j);
+        //     max= Math.max(max,sum);
+        //     i++;
+        //     j--;
+        // }
+        // return max;
+
+        ListNode slow=head;
+        ListNode fast=head;
+
+        while(fast!=null && fast.next!=null)
         {
-            int sum=list.get(i)+list.get(j);
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        ListNode p2= reverse(slow);
+        ListNode p1=head;
+        int max=0;
+        while(p1!=null && p2!=null)
+        {
+            int sum= p1.val+p2.val;
             max= Math.max(max,sum);
-            i++;
-            j--;
+            p1=p1.next;
+            p2=p2.next;
         }
         return max;
     }
