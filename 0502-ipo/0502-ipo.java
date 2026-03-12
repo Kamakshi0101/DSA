97272dd9e5a4cbc8eb8656c02129b7fdfc8b41a0
Kamakshi Aggarwal <138430584+Kamakshi0101@users.noreply.github.com>
2026-03-13 00:24:45 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0502-ipo/0502-ipo.java
+++ 0502-ipo/0502-ipo.java
@@ -1,26 +1,54 @@
+// class Solution {
+//     public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
+//         PriorityQueue<Integer> maxheap= new PriorityQueue<>(Collections.reverseOrder());
+//         int[][] project= new int[profits.length][2];
+
+//         for(int i=0;i<profits.length;i++)
+//         {
+//             project[i][0]=capital[i];
+//             project[i][1]=profits[i];
+//         }
+//         Arrays.sort(project,(a,b)->Integer.compare(a[0],b[0]));
+//         int i=0;
+//         while(k-->0)
+//         {
+//             while(i< profits.length && project[i][0]<=w)
+//             {
+//                 maxheap.offer(project[i][1]);
+//                 i++;
+//             }
+//             if(maxheap.isEmpty()) break;
+
+//             w+=maxheap.poll();
+//         }
+//         return w;
+//     }
+// }
+
 class Solution {
     public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
-        PriorityQueue<Integer> maxheap= new PriorityQueue<>(Collections.reverseOrder());
-        int[][] project= new int[profits.length][2];
 
-        for(int i=0;i<profits.length;i++)
+        PriorityQueue<int[]> minCapital = new PriorityQueue<>((a,b) -> a[0] - b[0]);
+        PriorityQueue<Integer> maxProfit = new PriorityQueue<>(Collections.reverseOrder());
+
+        for(int i = 0; i < profits.length; i++)
         {
-            project[i][0]=capital[i];
-            project[i][1]=profits[i];
+            minCapital.offer(new int[]{capital[i], profits[i]});
         }
-        Arrays.sort(project,(a,b)->Integer.compare(a[0],b[0]));
-        int i=0;
-        while(k-->0)
+
+        while(k-- > 0)
         {
-            while(i< profits.length && project[i][0]<=w)
+            while(!minCapital.isEmpty() && minCapital.peek()[0] <= w)
             {
-                maxheap.offer(project[i][1]);
-                i++;
+                maxProfit.offer(minCapital.poll()[1]);
             }
-            if(maxheap.isEmpty()) break;
 
-            w+=maxheap.poll();
+            if(maxProfit.isEmpty())
+                break;
+
+            w += maxProfit.poll();
         }
+
         return w;
     }
 }
