13eb4f77769b4ae69f0c04e2630f874af50a1aae
Kamakshi Aggarwal <138430584+Kamakshi0101@users.noreply.github.com>
2026-05-07 22:08:50 +0530

    Time: 2 ms (98.29%), Space: 43.7 MB (52.86%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
@@ -1,5 +1,12 @@
 class Solution {
     public boolean uniqueOccurrences(int[] arr) {
-        
+        Map<Integer, Integer> freq = new HashMap<>();
+        for(int i: arr) freq.put(i, freq.getOrDefault(i, 0) + 1);
+        Set<Integer> set = new HashSet<>();
+        for(int i: freq.values()) {
+            if(set.contains(i)) return false;
+            set.add(i);
+        }
+        return true;
     }
 }
